3ae6ce8e1471fcb10dba579dbbc1a669d0fdab8b
Chumpol Mokarat <chumpol_mo@rmutto.ac.th>
2023-02-09 10:55:22 +0700

    Convert folders to zip file.
    Convert folders to zip file.

--- ch042/js/opentab.js
+++
@@ -1,9 +0,0 @@
-/* Source: https://www.w3schools.com/w3css/w3css_tabulators.asp */
-function openCity(cityName) {
-  var i;
-  var x = document.getElementsByClassName("city");
-  for (i = 0; i < x.length; i++) {
-    x[i].style.display = "none";  
-  }
-  document.getElementById(cityName).style.display = "block";  
-}
